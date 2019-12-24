241b13e653ea73d21a84ad35d0f00aa4c816487a
Aman Mathur <25167612+wolfoxx@users.noreply.github.com>
2019-12-24 11:34:05 +0530

    webpack-dev-server setup done!

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path=require('path');
+const HtmlWebpackPlugin=require('html-webpack-plugin');
+
+module.exports= {
+    entry:'./src/js/index.js',
+    output:{
+        path:path.resolve(__dirname,'dist/js'),
+        filename: 'bundle.js'
+    },
+    devServer:{
+        contentBase:'./dist'
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            filename:'index.html',
+            template:'./src/index.html'
+        })
+    ]
+}
+ 
