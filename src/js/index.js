a09751e82ce6ff60ed759b666035712d913fa640
Aman Mathur <25167612+wolfoxx@users.noreply.github.com>
2020-01-16 11:15:48 +0530

    built list model

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,8 @@
 
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
+
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import { elements,renderLoader,clearLoader } from './views/base';
@@ -141,7 +143,8 @@ elements.recipe.addEventListener('click',e=>{
 });
 
 
-
+const l=new List();
+window.l=l;
 
 
 
